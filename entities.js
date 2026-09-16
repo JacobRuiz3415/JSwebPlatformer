2babdcde6d1e1d868117f6013ae2d77f7035da8c
jacobruiz428 <jacobruiz428@gmail.com>
2026-09-16 16:16:40 -0700

    coment

--- entities.js
+++ entities.js
@@ -11,10 +11,14 @@ var PlayerEntity = me.ObjectEntity.extend({
     if (me.input.isKeyPressed('jump')) { this.doJump(); }
     me.game.collide(this);
     this.updateMovement();
+    if (this.bottom > 490){ this.gameOver(); }
     if (this.vel.x!=0 || this.vel.y!=0) {
       this.parent(this);
       return true;
     }
     return false;
+  }, // Don't forget to add this comma here
+  gameOver: function() {
+    me.state.change(me.state.MENU);
   }
-});
+}); //end of file
--- main.js
+++ main.js
@@ -10,10 +10,11 @@ var jsApp = {
   },
   loaded: function() {
     me.entityPool.add("player", PlayerEntity);
-    me.state.set(me.state.PLAY, new PlayScreen());
-    me.state.change(me.state.PLAY);
+    me.state.set(me.state.MENU, new TitleScreen());
+    me.state.transition("fade", "#2FA2C2", 250);
+    me.state.change(me.state.MENU);
   }
 };
 window.onReady(function() {
   jsApp.onload();
-});
+}); //end of file
